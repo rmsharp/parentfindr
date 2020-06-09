644c92bcd8fc32989a747d8f415b47be77ff157f
rmsharp <rmsharp@me.com>
2020-06-08 23:43:29 -0500

    added unit test for microsatelite-based allele file

--- tests/testthat/test_getAnimalAllelesDfFromCsv.R
+++ tests/testthat/test_getAnimalAllelesDfFromCsv.R
@@ -4,7 +4,8 @@ library(testthat)
 library(parentfindr)
 context("getAnimalAllelesDfFromCsv")
 
-test_that("getAnimalAllelesDfFromCsv reads CSV files.", {
+test_that(paste0("getAnimalAllelesDfFromCsv reads SNP-based allele ",
+                   "file in CSV format."), {
   animalAllelesDf <-
     getAnimalAllelesDfFromCsv(
       alleleFile = system.file("testdata", "snp-animal-alleles.txt",
@@ -17,6 +18,24 @@ test_that("getAnimalAllelesDfFromCsv reads CSV files.", {
                  "43927", "43601", "44540", "44696", "43383", "48858", "48764",
                  "47680", "48741", "48651", "48642", "48851", "43307", "47728",
                  "48955", "47520", "48799"))
+  expect_equal(animalAllelesDf$`43336`$sex, "F")
   expect_equal(animalAllelesDf$`43336`$birthDate,
                structure(13997, class = "Date"))
+})
+test_that(paste0("getAnimalAllelesDfFromCsv reads Microsatellite-based allele ",
+                 "file in CSV format."), {
+  animalAllelesDf <-
+    getAnimalAllelesDfFromCsv(
+      alleleFile = system.file("testdata", "str-animal-alleles.txt",
+                               package = "parentfindr"),
+      dateType = "YYYYMMDD")
+  expect_equal(length(animalAllelesDf), 25)
+  expect_equal(names(animalAllelesDf),
+               c("AN1", "AN2", "AN3", "AN4", "AN5", "AN6", "AN7", "AN8", "AN9",
+                 "AN10", "AN11", "AN12", "AN13", "AN14", "AN15", "AN16", "AN17",
+                 "AN18", "AN19", "AN20", "AN21", "AN22", "AN23", "AN24", "AN25"
+               ))
+  expect_equal(animalAllelesDf$`AN5`$sex, "M")
+  expect_equal(animalAllelesDf$`AN5`$birthDate,
+               structure(NA_real_, class = "Date"))
 })
